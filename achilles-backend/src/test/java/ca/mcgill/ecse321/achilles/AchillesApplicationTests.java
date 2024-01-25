f034cc1c7ec06cb2116cfa8aed27734e3b9d101b
wangwiza <william.wang4@mail.mcgill.ca>
2024-01-25 16:28:54 -0500

    (init): spring init java 21 with dev dependencies

---
+++ achilles-backend/src/test/java/ca/mcgill/ecse321/achilles/AchillesApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.achilles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchillesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
