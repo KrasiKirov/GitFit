d84d1f58395c4ec3fc776f05b03fc03ac366c5ee
Kevin Li <kevin-ruikai.li@mail.mcgill.ca>
2024-02-15 15:23:34 -0500

    changed gitfit to gitfit-backend. added package to model classes.

--- gitfit-backend/src/main/java/ca/mcgill/ecse321/gitfit/model/Billing.java
+++ gitfit-backend/src/main/java/ca/mcgill/ecse321/gitfit/model/Billing.java
@@ -1,10 +1,5 @@
-/*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.33.0.6934.a386b0a58 modeling language!*/
+package ca.mcgill.ecse321.gitfit.model;
 
-
-
-// line 54 "model.ump"
-// line 89 "model.ump"
 public class Billing
 {
 
