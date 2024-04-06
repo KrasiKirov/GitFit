5748a8dd7cdb4dd2f28240e795b025ca357b2903
wangwiza <76924804+wangwiza@users.noreply.github.com>
2024-04-05 20:49:55 -0400

    (fix): surprise ;)

--- gitfit-frontend/src/views/HomeView.vue
+++ gitfit-frontend/src/views/HomeView.vue
@@ -17,9 +17,20 @@ const refreshClasses = async () => {
 </script>
 
 <template>
-    <h1 class="text-3xl font-semibold text-center my-8">Good morning</h1>
-    <div class="grid grid-cols-3 gap-4">
+    <div class="bg-gradient-to-r from-persianblue via-moodyblue to-spindle text-white text-center py-20">
+        <h1 class="text-6xl font-bold">Good morning</h1>
+    </div>
+    <div class="grid grid-cols-3 gap-8 p-8">
         <FitnessClassCard v-for="fclass in fitnessClasses" :key="fclass.name" :fitnessClass="fclass"
-            @update="refreshClasses" />
+            @update="refreshClasses"
+            class="transform transition-transform duration-500 hover:scale-105 bg-linkwater rounded-lg shadow-md" />
     </div>
 </template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
+body {
+    font-family: 'Inter', sans-serif;
+}
+</style>
